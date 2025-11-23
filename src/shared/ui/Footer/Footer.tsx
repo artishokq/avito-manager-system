8d8e246013fa1d59f6ce86661cc52fae40e6c6ab
artishokq <astkachuk_2@edu.hse.ru>
2025-11-23 12:51:38 +0300

    добавил header и footer, подключил MUI

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -1,5 +1,22 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Тестовый проект выполнил: Ткачук Артём (github:{" "}
+        <a
+          className={styles.link}
+          href="https://github.com/artishokq/avito-manager-system/tree/main"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @artishokq
+        </a>
+        )
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,5 +1,13 @@
+import styles from "./Header.module.css";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        Система управления объявлениями для модерации
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
